bf7dedee93d150b2b19913a4535fd09eb10ab5ec
Henrik Vollesen <henrik@vollesen.dk>
2023-04-10 07:13:37 +0200

    Removed unneccessary Usings

--- DAL/ChapterService.cs
+++ DAL/ChapterService.cs
@@ -1,11 +1,6 @@
 ﻿using OneLoopDAL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Nodes;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace OneLoopDAL.DAL
 {
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OneLoopDAL.Models
+﻿namespace OneLoopDAL.Models
 {
     public class Book
     {
         public string _id { get; set; } = "";
         public string name { get; set; } = "";
-        //public string Id { get; set; }
-        //public string Name { get; set; }
     }
 }
